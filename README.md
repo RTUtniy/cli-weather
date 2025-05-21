e15010f201e623b6918988d669fc75db966dd0cf
RTUtniy <iaroslav.viazmitin@edu.rtu.lv>
2025-05-21 09:36:10 +0300

    Mēs darījām visu Replitā un tāpēc šeit nebūs citu komitu

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# cli-weather
-Laikapstākļi Ķīpsalas studentu pilsētiņā
+# `cli-weather`
+
+`cli-weather` ir programmatūra, kas ļauj sekot dažiem konkrētiem laikapstākļiem iepriekš izvēlētajā vietā ─ Ķīpsalas studentu pilsētiņā ─ komandrindā. Pēc būtības šāds projekts ir papildināms, uzlabojams vai pielagojams citu cilvēku vajadzībām. Piemēram, ja kādam neinteresē spiediena rādītājs, var ātri nomainīt programmatūras pirmkodu tā, lai tāda parametra nebūtu, vai arī pievienot citus.
+
+## Datu struktūra "Tunelis"
+
+Domājot par to, kā šo struktūru varētu nosaukt, uzreiz izveidojās asociācija ar tuneli kalnā vai zemes paaugurā, kurā brauc vilciens. Vilcieni parasti vienā līnijā brauc tikai uz vienu pusi, bet tunelī, it īpaši mazā, viņiem var būt redzams gan sākums, gan beigas, bet vidējie vagoni paliks tunelī. Līdzīgi arī strādā šis tunelis. Tunelim ir ierobežots glabājamo objektu skaits (šajā uzdevumā, tas ir 7, kas sakrīt ar nedēļas dienu skaitu), elementi ir vienvirzieni saistīti. Mēģinot pārpildīt struktūru, vecākais elements (tas, kurš tika pievienots agrāk par citiem, šajā gadījumā tas ir struktūras `head` elements) tiek aizstāts ar tam sekojošo elementu (šādā veidā tiek atbrīvota vieta vēl vienam elementam), bet struktūras beigās tiek pievienots jauns elements ─ gluži kā vagoni, kas brauc tunelī. 
+
+```
+pirms | iekš | aiz (tuneļa)
+DBC   | A    |
+ CD   | BA   |
+  D   | CB   | A
+      | DC   | BA
+```
+
+Nelielā shematiskajā attēlā virs ir vizualizēts strukrūras darbības princips, kur katrs burts ir struktūras elements. `head` elements vienmēr atrodas pa labi no visiem citiem elementiem šajā struktūras vizuālizācijā.
+
+## Programmatūras loģika
+
+Pēc programmas inicializācijas (104. r.), tiek izveidota laikapstāķļu datu glabātuve `weather_store` un startēta funkcija `main`, kurā notiek visi galvenie uzdevumi. Funkcija `test` netiek lietota, bet tā tika izmantota sākumā, lai pārbaudītu, vai pareizi strādā jaunā datu struktūra.
+
+Izsauktajā funkcijā tiek veidota tabula ar *Rich* bibliotēkas instrumentiem un pēc katras stundas tās attēls tiek atsvaidzināts ─ tabula netiek izvadīta vēlreiz, bet tās dati tiek pārrakstīti. 
+
+Katru reizi atsvaidzinot tabulu, tiek izveidots pieprasījums uz laikapstākļu lietojumprogrammu saskarni (angl. *API*), lai iegūtu datus par laikapstākļiem Ķīpsalas studentu pilsētiņā. Pēc veiksmīgā pieprasījuma, dati tiek apstrādāti, ņemot vērā, ka tie ir par katru stundu visām septiņām dienām. Dati par katru dienu ērtākai piekļuvei ir apkopoti vārdnīcā un vārdnīcas par visām dienām tiek augšuplādētas `weather_store` struktūrā. Tad tabula tiek formatēta un dienu datumi tiek ģenerēti ar `datetime` bibliotēkas funkciju palīdzību. Pēc tam dati par nokrišņiem tiek izņemti no struktūras, pārgrupēti no dienu vārdnīcām pēc kritērijiem par kritēriju kortēžiem pēc dienām. Kortēžs tiek izpakots pirms jaunas datu rindas pievienošanas tabulā un tas atkārtojas katram kritērijam.
+
+Programmā nav nodrošināta tās pārtraukšana pēc lietotāja ievada, tā jāpartrauc ar ārējām metodēm ─ komandrindas loga aizvēršana vai ārkārta pārtraukšana ar `Ctrl+C` īsinājumtaistiņu.
+
+![https://viewer.diagrams.net/?tags=%7B%7D&lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=test.drawio&dark=auto#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1WugK82UB-sQVugWjHt1BSKgfmmq-E-1w%26export%3Ddownload](./logic.png)
+
+## Lietotās bibliotēkas
+
+- `requests` ─ datu saņemšanai no lietojumprogrammu saskarnes
+- `time` ─ lai apturēt programmu uz stundu, pirms tā atsvaidzinās tabulu ar datiem
+- `datetime` ─ īsākai datumu formatēšanai
+- `rich` ─ datu formatēšanai komandrindā un ērtākai izvadīšanai (lai neizvadītu visu tabulu no jauna, bet tikai pārrakstītu datus)
+
+## Izmantošanās metodes
+
+Ja kāds dzīvo studentu pilsētiņā, viņš var datora darbvirsmas stūrī uzlikt logrīku ar komandrindu, kurā tiks izvadīta informācija par laikapstākļiem.
+
+---
+
+[Iaroslav Viazmitin](https://github.com/RTUtniy), [Daniels Trunovs](https://github.com/danielstrunovs)
---
+++ main.py
@@ -0,0 +1,122 @@
+import requests
+import time
+import datetime
+
+from rich.live import Live
+from rich.table import Table
+
+class Node:
+    """Nods, kas satur datus par laikapstākļiem un norādi uz nākamo un iepriekšējo nodu."""
+    def __init__(self, value):
+        self.value = value
+        self.next: None | Node = None
+
+class Tunnel:
+    """Datu struktūra, kurai ir piekļuve sākumam un beigām."""
+    __SIZE: int = 7 # (private) nedēļas dienu skaits
+    def __init__(self):
+        self.size: int = 0
+        self.head: None | Node = None
+
+    def add(self, data):
+        """Pievieno jaunu nodu beigās un izdzēs sākuma nodu, lai nebūtu pārpildīšanas."""
+        new_node = Node(data)
+        if self.head is None: # ja tunelis ir tukšs
+            self.head = new_node
+            self.size += 1
+            return
+        elif self.size == self.__SIZE: # ja tunelis ir pilns
+            self.head = self.head.next
+            iter_node = self.head
+            while iter_node.next is not None:
+                iter_node = iter_node.next
+            iter_node.next = new_node
+            return
+        else: # ja tunelis ir daļēji pilns
+            iter_node = self.head
+            while iter_node.next is not None:
+                iter_node = iter_node.next
+            iter_node.next = new_node
+            self.size += 1
+
+def test():
+    """Datu simulācija datu struktūras testiem, pirms API pieslēgšanas"""
+    temporary_data = ["10 C", "12 C", "14 C", "16 C", "18 C", "20 C", "22 C", "24 C"]
+    
+    for data in temporary_data:
+        weather_store.add(data)
+        iter_node = weather_store.head
+        while iter_node is not None:
+            print(iter_node.value, "COUNTER")
+            iter_node = iter_node.next
+        print("----")
+
+def main(API_URL: str):
+    """prod"""
+
+    table = Table(title="Laikapstākļi Ķīpsalas studentu pilsētiņā")
+    def update_data() -> None:
+        """Atjaunina datus no API un saglabā tos datu struktūrā."""
+        global weather_store
+        response = requests.get(API_URL)
+        if response.ok:
+            response = response.json()
+        else:
+            raise Exception("API pieprasījums neizdevās.")
+
+        criteria_names = ["Vidējā temp.", "Nokrišņu kopā", "Vid. spiediens"]
+        
+        for day in range(7):
+            day_data = {
+                "Vidējā temp.": 0.0,
+                "Nokrišņu kopā": 0.0,
+                "Vid. spiediens": 0.0
+            }
+            for hour in range(24):
+                day_data["Vidējā temp."] += response["hourly"]["temperature_2m"][day*24 + hour]
+                day_data["Nokrišņu kopā"] += response["hourly"]["rain"][day*24 + hour]
+                day_data["Vid. spiediens"] += response["hourly"]["surface_pressure"][day*24 + hour] * 0.75 # konvertē hPa -> mmHg
+                if hour == 23:
+                    day_data["Vidējā temp."] /= 24
+                    day_data["Vid. spiediens"] /= 24
+                    weather_store.add(day_data)
+        
+        table.add_column("Mērvienība.", no_wrap=True, style="cyan")
+        table.add_column("Šodien", no_wrap=True, justify="right", style="magenta")
+        table.add_column("Rīt", no_wrap=True, justify="right", style="magenta")
+        date = datetime.datetime.today() + datetime.timedelta(days=1)
+        for day in range(5):
+            """data format: dd.mm.yy"""
+            date = date + datetime.timedelta(days=1)
+            table.add_column(date.strftime("%d.%m.%y"), no_wrap=True, justify="right", style="magenta")
+
+        iter_node = weather_store.head
+        criteria_measures = [" °C", " mm", " mmHg"]
+        for index in range(3):
+            criteria_data: tuple = tuple()
+            for day in range(7):
+                current_data = str(
+                    round(
+                        iter_node.value[criteria_names[index]]
+                    )
+                ) + criteria_measures[index]
+                criteria_data += (current_data,) 
+                iter_node = iter_node.next
+            table.add_row(criteria_names[index], *criteria_data)
+            iter_node = weather_store.head
+        
+
+    with Live(table, refresh_per_second=60*60) as live:
+        while True:
+            update_data()
+            time.sleep(60*60)
+            live.update(table)
+
+if __name__ == "__main__":
+    weather_store = Tunnel()
+    # test()
+    main("https://api.open-meteo.com/v1/forecast\
+?latitude=56.9530072\
+&longitude=24.0814191\
+&hourly=temperature_2m,rain,surface_pressure\
+&timezone=auto")
